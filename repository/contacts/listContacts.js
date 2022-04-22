5a5bdfa95e89394e3346683933bde6abff917708
YaroslavLazorenko <lazorenko85@gmail.com>
2022-04-23 01:05:50 +0300

    Add favorite query handling

--- repository/contacts/listContacts.js
+++ repository/contacts/listContacts.js
@@ -3,8 +3,16 @@ const Contact = require("../../models/contact");
 async function listContacts(req, res) {
   const { _id } = req.user;
   const { page = 1, limit = 10 } = req.query;
+  const { favorite } = req.query;
+
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id }, "", {
+  let filter = null;
+  if (favorite === "false" || favorite === "true") {
+    const isFavoriteTrue = favorite === "true";
+    filter = { favorite: isFavoriteTrue };
+  }
+
+  const contacts = await Contact.find({ owner: _id, ...filter }, "", {
     skip,
     limit: Number(limit),
   }).populate("owner", "_id email");
